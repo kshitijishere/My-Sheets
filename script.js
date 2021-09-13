57d330ebdff9f6b61e92a8b16150c5cd18e11837
kshitij <kshitijvats09121998@gmail.com>
2021-09-14 03:47:34 +0530

    day 2

--- script.js
+++ script.js
@@ -41,6 +41,61 @@ for(let i=1;i<=100;i++)
     div.classList.add('firstcoldiv');
     firstcol.append(div);
 }
+const toprow=document.querySelector('.firstrow');
+for(var i=1;i<=26;i++)
+{
+    const div=document.createElement('div');
+    div.innerText=String.fromCharCode(64+i);
+    div.classList.add('firstrowdiv');
+    toprow.append(div);
+}
+const grid=document.querySelector('.grid');
+for(let i=1;i<=100;i++)
+{
+    const row=document.createElement('div');
+    row.innerText;
+    row.classList.add('rows');
+    for(let j=1;j<=26;j++)
+    {
+        const div=document.createElement('div');
+        div.classList.add('cells');
+        div.setAttribute('col',j);
+        div.setAttribute('row',i);
+        div.setAttribute('contenteditable',"true");
+        // div.innerText=`${String.fromCharCode(64+j)}${i}`;
+        div.addEventListener('click',()=>{
+            const display=document.querySelector('#selectedcell');
+            
+            display.value=`${String.fromCharCode(64+j)}${i}`
+        })
+        
+        row.append(div);
+    }
+    grid.append(row);
+}
 
 
+
+// script for menu
+
+let bold=document.querySelector('#span1');
+bold.addEventListener('click',()=>{
+    bold.classList.add('iconactive');
+        let ad=document.querySelector('#selectedcell');
+        
+        let colno=ad.value.charCodeAt(0);
+        colno=colno-64;
+        colno=colno.toString();
+        let rowno="";
+        for(let i=1;i<ad.value.length;i++)
+        {
+            rowno=rowno+ad.value[i];
+        }
+
+        rowno=rowno.toString();
+        
+        let celldiv=document.querySelector(`div[col="${colno}"][row="${rowno}"]`);
+    
+    celldiv.style.fontWeight="bold";
+})
 
